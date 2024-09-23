a43f9de7cb848d689df1388f888d107f8752f445
bhargavratnala <bhargavratnala2004@gmail.com>
2024-09-23 22:08:43 +0530

    unsupported message for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import pcBg from "./assets/images/pcBg.jpeg";
 import Desktop from "./components/desktop";
 
 function App() {
-
+	const userAgent = navigator.userAgent || navigator.vendor;
+	if (/android|iPad|iPhone|iPod|webOS|BlackBerry|Windows Phone/i.test(userAgent))
+		return (
+			<div className="mobileError">
+				<h1>Mobile Not Supported</h1>
+				<p>This website is not supported on mobile devices. Please open this website on a desktop or laptop.</p>
+			</div>
+		);
   return (
 	<>
 		<SuspenseLoader>
